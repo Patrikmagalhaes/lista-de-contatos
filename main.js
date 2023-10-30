1f1608ca90066b87acf2187e7c8ab2bbed90400f
patrikmagalhaes <patrikmagalhaes@icloud.com>
2023-10-29 22:35:12 -0300

    adicionando img

--- main.js
+++ main.js
@@ -2,26 +2,40 @@ const form = document.getElementById("formulario-de-contato")
 const nome = []
 const numero = []
 let linhas = ""
+
 form.addEventListener("submit", function (e) {
     e.preventDefault()
 
-    const inputNome = document.querySelector("#nome-contato").value
-    const inputNumero = document.querySelector("#numero-contato").value
+    const inputNome = document.querySelector("#nome-contato")
+    const inputNumero = document.querySelector("#numero-contato")
+   
 
-    nome.push(inputNome)
-    numero.push(parseFloat(inputNumero))
+    if (numero.includes(parseFloat(inputNumero.value))) {
+        alert("Esse numero ja esta registrado")
+        inputNome.value = ""
+        inputNumero.value = ""
+    }
+    else {
+        nome.push(inputNome.value)
+        numero.push(parseFloat(inputNumero.value))
 
-    let linha = '<tr>'
-    linha += `<td>${nome[nome.length - 1]}</td>`
-    linha += `<td>${numero[nome.length - 1]}</td>`
-    linha += '</tr>'
+        let linha = '<tr>'
+        linha = + `<label for="img" class="picture"></label>`
+        linha += `<td>${nome[nome.length - 1]}</td>`
+        linha += `<td>${numero[nome.length - 1]}</td>`
+        linha += '</tr>'
 
-    linhas += linha
+        linhas += linha
 
-    const corpotabela = document.querySelector('tbody')
-    corpotabela.innerHTML = linhas
+        const corpotabela = document.querySelector('tbody')
+        corpotabela.innerHTML = linhas
+
+        inputNome.value = ""
+        inputNumero.value = ""
+    }
 
 })
+
 
 
 
